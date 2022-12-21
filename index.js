89f5fd96178387b8734242b8e41feed63a52ec1b
Tushar-Banswal <tusharbanswal2003@gmail.com>
2022-12-21 15:36:13 +0530

    Adding websocket server

--- index.js
+++ index.js
@@ -1,53 +1,32 @@
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server,{
+    cors: {
+        origin: "http://127.0.0.1:5500",
+        methods: ["GET", "POST"]
+      }
+});
 
-const socket = io('http://localhost:3000');
+const users = {};
 
-const form = document.querySelector('#sendMessage');
-const message = document.querySelector('#messageInp');
-const msgContainer = document.querySelector('.message-cont')
+io.on('connection', (socket) => {
+    socket.on('newUserAdd', (name) => {
+        users[socket.id] = name;
+        socket.broadcast.emit('userJoined', name);
+    })
 
-const userName = prompt("Enter your name: ");
-socket.emit('newUserAdd', userName);
+    socket.on('send', (message) => {
+        socket.broadcast.emit('receive', {message: message, name: users[socket.id]});
+    })
 
-const joined = (name) => {
-    const container = document.createElement('p');
-    container.innerText = name + " joined the chat";
-    container.classList.add('joined');
-    msgContainer.appendChild(container);
-}
+    socket.on('disconnect', (message) => {
+        socket.broadcast.emit('left', users[socket.id]);
+        delete users[socket.id];
+    });
 
-const append = (message, position) => {
-    const container = document.createElement('p');
-    container.innerText = message;
-    container.classList.add('msg');
-    container.classList.add(position);
-    msgContainer.appendChild(container);
-}
-
-const left = (name)=>{
-    const container = document.createElement('p');
-    container.innerText = name + " left the chat";
-    container.classList.add('joined');
-    msgContainer.appendChild(container);
-}
-
-socket.on('userJoined', (name) => {
-    joined(name);
 })
 
-socket.on('receive', (data) => {
-    const msg = data.message;
-    const sender = data.name;
-    append(`${sender}: ${msg}`, 'left');
-}) 
+server.listen(3000);
 
-socket.on('left', (name) => {
-    left(name);
-})
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const msg = message.value;
-    append(`You: ${msg}`, 'right');
-    socket.emit('send', msg);
-    message.value = "";
-})
+
